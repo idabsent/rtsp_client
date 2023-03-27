d79dd17ebf50d090694d091cf8b940a964bac59f
emptyty <idabsentgi@gmail.com>
2023-03-27 22:52:30 +0300

    Implementation From header

---
+++ src/headers/from.rs
@@ -0,0 +1,44 @@
+use uriparse::URI;
+
+use crate::{
+    headers::interface::{Header, HeaderPosition},
+    requests::interface::RequestMethod,
+};
+
+pub struct From<'a> {
+    uri: URI<'a>
+}
+
+impl<'a> From<'a> {
+    fn new(uri: URI<'a>) -> From {
+        From {
+            uri,
+        }
+    }
+
+    fn set_uri(&mut self, uri: URI<'a>) {
+        self.uri = uri
+    }
+}
+
+impl<'a> Header for From<'a> {
+    fn header() -> String {
+        String::from("From")
+    }
+
+    fn allow_in_methods() -> &'static [RequestMethod] {
+        &[RequestMethod::Describe, RequestMethod::Setup,
+          RequestMethod::Play, RequestMethod::Pause,
+          RequestMethod::Teardown, RequestMethod::Options,
+          RequestMethod::GetParameter, RequestMethod::SetParameter,
+          RequestMethod::Redirect]
+    }
+
+    fn header_position() -> HeaderPosition {
+        HeaderPosition::RequestResponse
+    }
+
+    fn value(&self) -> String {
+        self.uri.to_string()
+    }
+}
