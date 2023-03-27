ebdab9ec12c4d2ee17eaea69c8de4547427cfab5
emptyty <idabsentgi@gmail.com>
2023-03-27 19:24:52 +0300

    Implementation Content-Length header

---
+++ src/headers/content_length.rs
@@ -0,0 +1,42 @@
+use crate::{
+    headers::interface::{Header, HeaderPosition},
+    requests::interface::{RequestMethod},
+};
+
+pub struct ContentLength {
+    length: u32,
+}
+
+impl ContentLength {
+    fn new(length: u32) -> ContentLength {
+        ContentLength {
+            length,
+        }
+    }
+
+    fn set_length(&mut self, length: u32) {
+        self.length = length;
+    }
+}
+
+impl Header for ContentLength {
+    fn header() -> String {
+        String::from("Content-Length")
+    }
+
+    fn allow_in_methods() -> &'static [RequestMethod] {
+        &[RequestMethod::Describe, RequestMethod::Options,
+          RequestMethod::Setup, RequestMethod::Play,
+          RequestMethod::Pause, RequestMethod::Teardown,
+          RequestMethod::SetParameter, RequestMethod::GetParameter,
+          RequestMethod::Redirect, RequestMethod::PlayNotify]
+    }
+
+    fn header_position() -> HeaderPosition {
+        HeaderPosition::MessageBody
+    }
+
+    fn value(&self) -> String {
+        self.length.to_string()
+    }
+}
