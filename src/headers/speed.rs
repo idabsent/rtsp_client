89413e09d66b1ba5d3468a43aec2dac32dfb9e7a
islam gamzaev <islam.gamzaev@azimut.ru>
2023-03-29 18:29:59 +0300

    Implementation Speed header

---
+++ src/headers/speed.rs
@@ -0,0 +1,55 @@
+use crate::{
+    headers::interface::{Header, HeaderPosition},
+    requests::interface::RequestMethod,
+};
+
+use std::ops::RangeInclusive;
+
+pub struct Speed {
+    range: RangeInclusive<f32>,
+}
+
+impl Speed {
+    fn new(range: RangeInclusive<f32>) -> Speed {
+        Speed {
+            range,
+        }
+    }
+
+    fn set_session(&mut self, range: RangeInclusive<f32>) {
+        self.range = range
+    }
+}
+
+impl Header for Speed {
+    fn header() -> String {
+        String::from("Session")
+    }
+
+    fn allow_in_methods() -> &'static [RequestMethod] {
+        &[RequestMethod::Setup, RequestMethod::Play,
+          RequestMethod::Options, RequestMethod::Teardown,
+          RequestMethod::GetParameter, RequestMethod::SetParameter,
+          RequestMethod::Redirect, RequestMethod::PlayNotify,
+          RequestMethod::Pause,]
+    }
+
+    fn header_position() -> HeaderPosition {
+        HeaderPosition::General
+    }
+
+    fn value(&self) -> String {
+        format!("{:.1}-{:.1}", self.range.start(), self.range.end())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_string_repr() {
+        let s_header = Speed::new(1.01..=2.01);
+        assert_eq!(&s_header.value(), "1.0-2.0")
+    }
+}
