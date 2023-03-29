0b13ecc4c1ec40e9601d7cee60bf46cdabf93a7a
islam gamzaev <islam.gamzaev@azimut.ru>
2023-03-29 17:15:56 +0300

    Implementation Scale header

---
+++ src/headers/scale.rs
@@ -0,0 +1,40 @@
+use crate::{
+    headers::interface::{Header, HeaderPosition},
+    requests::interface::RequestMethod,
+};
+
+pub struct Scale {
+    scale: i8,
+}
+
+impl Scale {
+    fn new(scale: i8) -> Scale {
+        Scale {
+            scale,
+        }
+    }
+
+    fn set_scale(&mut self, scale: i8) {
+        self.scale = scale;
+    }
+}
+
+impl Header for Scale {
+    fn header() -> String {
+        String::from("Scale")
+    }
+
+    fn allow_in_methods() -> &'static [RequestMethod] {
+        &[RequestMethod::Setup, RequestMethod::Play,
+          RequestMethod::Options, RequestMethod::GetParameter,
+          RequestMethod::Redirect, RequestMethod::PlayNotify]
+    }
+
+    fn header_position() -> HeaderPosition {
+        HeaderPosition::General
+    }
+
+    fn value(&self) -> String {
+        self.scale.to_string()
+    }
+}
