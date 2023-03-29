7dc8ec088b82e014cb4e9a76825935095ba5e374
islam gamzaev <islam.gamzaev@azimut.ru>
2023-03-29 18:07:20 +0300

    Implementation Session header

---
+++ src/headers/session.rs
@@ -0,0 +1,42 @@
+use crate::{
+    headers::interface::{Header, HeaderPosition},
+    requests::interface::RequestMethod,
+};
+
+pub struct Session {
+    session_id: String,
+}
+
+impl Session {
+    fn new(session_id: String) -> Session {
+        Session {
+            session_id,
+        }
+    }
+
+    fn set_session(&mut self, session_id: String) {
+        self.session_id = session_id
+    }
+}
+
+impl Header for Session {
+    fn header() -> String {
+        String::from("Session")
+    }
+
+    fn allow_in_methods() -> &'static [RequestMethod] {
+        &[RequestMethod::Setup, RequestMethod::Play,
+          RequestMethod::Options, RequestMethod::Teardown,
+          RequestMethod::GetParameter, RequestMethod::SetParameter,
+          RequestMethod::Redirect, RequestMethod::PlayNotify,
+          RequestMethod::Pause,]
+    }
+
+    fn header_position() -> HeaderPosition {
+        HeaderPosition::General
+    }
+
+    fn value(&self) -> String {
+        self.session_id.to_string()
+    }
+}
